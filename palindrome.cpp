6a26c37b82a5fb62981387f1ccd37944daa35998
Jeremiah Mahler <jmmahler@gmail.com>
2011-05-18 12:04:34 -0700

    improved palindrome algorithm

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <stack>
+#include <queue>
 
 using namespace std;
 
@@ -9,10 +10,12 @@ using namespace std;
  *
  * USAGE:
  *
- * ./palindrom <word>
+ * ./palindrome <word>
  */
 
 string reverse(string);
+bool isPalindrome(string);
+bool isPalindrome2(string);
 
 int main(int argc, char** argv)
 {
@@ -21,13 +24,13 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
-	string word;
-	string word_orig;
+	string word;  	   // without spaces
+	string word_orig;  // with spaces
 
-	// join all the words in argv in to a single string separated by spaces
+	// build word and word_orig from **argv
 	for (int i = 1; i < argc; i++) {
 
-		// include spaces
+		// include spaces in original
 		if (i >= 2)
 	    	word_orig += " ";
 
@@ -35,15 +38,10 @@ int main(int argc, char** argv)
 		word_orig += argv[i];
 	}
 
-	string wordr;  // word in reverse stored here
-
-	wordr = reverse(word);
-
-	//cout << "The word '" << word << "' in reverse is '" << wordr << "'\n";
-
 	cout << "'" << word_orig << "' is ";
 
-	if (wordr != word)
+	//if (! isPalindrome(word))
+	if (! isPalindrome2(word))
 		cout << "NOT";
 
 	cout << " a palindrome\n";
@@ -51,6 +49,79 @@ int main(int argc, char** argv)
 	return 0;
 }
 
+// {{{ isPalindrome()
+/**
+ * Test wheter a string is or is not a palindrome.
+ *
+ * @arg string to test
+ *
+ * @returns true if it is a palindrome, false otherwise
+ *
+ * This version simply reverses the string and tests whether
+ * it is equal to the original string.
+ */
+bool isPalindrome(string s) {
+	string sr = reverse(s);
+
+	if (sr == s)
+		return true;
+	// else
+	return false;
+}
+// }}}
+
+// {{{ isPalindrome2()
+/**
+ * Test wheter a string is or is not a palindrome.
+ *
+ * @arg string to test
+ *
+ * @returns true if it is a palindrome, false otherwise
+ *
+ * This version does not have to scan through the entire
+ * string to detect if it is not a palindrome.
+ * If the first character past the half way point is different
+ * it would only have to perform length/2 + 1 steps.
+ * If it is a palindrome it still has to scan the entire string.
+ */
+bool isPalindrome2(string s) {
+	int len = s.length();
+	int mid = len / 2;
+	bool oddp = (0 != (len % 2));
+
+	stack<int> q;
+
+	int i = 0;
+	for (; i < mid; i++) {
+		q.push(s[i]);
+	}
+
+	if (oddp)
+		i += 1;  // skip the odd middle
+
+	for (; i < len; i++) {
+		char c = q.top();
+
+		if (c != s[i])
+			return false;
+
+		q.pop();
+	}
+
+	return true;
+}
+// }}}
+
+// {{{ reverse(string)
+/**
+ * Reverse all the characters in a string.
+ *
+ * @arg string to reverse
+ *
+ * @returns reversed string
+ *
+ * Uses a stack to accomplish reversion.
+ */
 string reverse(string word) {
 	stack<char> sk;
 	int size = word.size();
@@ -68,3 +139,5 @@ string reverse(string word) {
 
 	return wordr;
 }
+// }}}
+
